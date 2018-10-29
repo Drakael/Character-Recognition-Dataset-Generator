91625edaa3fabefd758c625703552e2f3c15a76e
Drakael <drakael@hotmail.com>
2018-10-29 17:45:32 +0100

    daily work...

--- image_reader.py
+++ image_reader.py
@@ -1,24 +1,27 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from keras.models import load_model
+from keras.models import Model, load_model
+from keras import layers, models, optimizers
 import json
 import sys
+from capsulelitelayers import CapsuleLayer, PrimaryCap, Length, Mask
 
 nb_channels = 1
 
 kernel_size = 24
 
-strides = 4
+strides = 6
 
 if nb_channels == 1:
     color_mode = 0
 else:
     color_mode = -1
 
-pic_path = 'Carte_identite_Louis_BEZ.jpg'
+pic_path = 'ticket_ratp_octobre_2018.jpg'
 
 img = cv2.imread(pic_path, color_mode)
+img = img / 255
 
 print('img.shape', img.shape)
 
@@ -37,6 +40,7 @@ nb_patch = int((((height - kernel_size) / strides) + 1) *
 
 print('nb_patch', nb_patch)
 
+dump_folder = 'dump_analysis/'
 batch = np.zeros((nb_patch, kernel_size, kernel_size))
 cnt = 0
 for i in range(0, height - kernel_size, strides):
@@ -48,26 +52,42 @@ for i in range(0, height - kernel_size, strides):
         #       'max', np.max(batch[cnt, ...]))
         cnt += 1
 
-for i in range(800, 805):
+for i in range(2900, 2905):
     plt.figure()
     plt.imshow(batch[i, ...], cmap='gray')
     plt.show()
 
 # sys.exit('die')
 
-model_name = 'OCR_French_CNN_64_adadelta_992.h5'
-# model_name = 'OCR_French_CapsNet.h5'
 
-model_dir = 'models_dump/'
-# model_dir = 'result/'
+def CapsNet(input_shape, n_class, routings):
+    """
+    A Capsule Network on MNIST.
+    :param input_shape: data shape, 3d, [width, height, channels]
+    :param n_class: number of classes
+    :param routings: number of routing iterations
+    :return: Two Keras Models, the first one used for training, and the second one for evaluation.
+            `eval_model` can also be used for training.
+    """
+    x = layers.Input(shape=input_shape)
 
-model = load_model(model_dir + model_name)
+    # Layer 1: Just a conventional Conv2D layer
+    conv1 = layers.Conv2D(filters=64, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
 
-batch = batch.reshape((batch.shape[0], batch.shape[1], batch.shape[2], 1))
+    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
+    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
 
-pred = model.predict(batch, verbose=1, batch_size=32)
+    # Layer 3: Capsule layer. Routing algorithm works here.
+    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=8, routings=routings,
+                             name='digitcaps')(primarycaps)
 
-predicted_class_indices = np.argmax(pred, axis=1)
+    # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
+    # If using tensorflow, this will not be necessary. :)
+    out_caps = Length(name='capsnet')(digitcaps)
+
+    # Models for training and evaluation (prediction)
+    return models.Model(x, out_caps)
+
 
 with open('char_list.json') as json_file:
     char_list = json.load(json_file)
@@ -76,6 +96,36 @@ print('char_list', char_list)
 
 with open('labels.json') as json_file:
     labels = json.load(json_file)
+
+# model = CapsNet(input_shape=(height, width, nb_channels),
+#                 n_class=len(char_list),
+#                 routings=3)
+# model.load_weights('weights-20e-0.985168.h5')
+
+
+model_name = 'OCR_French_CNN_64_adadelta_992.h5'
+# model_name = 'OCR_French_CapsNet.h5'
+
+# model_dir = 'models_dump/'
+# model_dir = 'result/'
+model = load_model(model_name)
+
+batch = batch.reshape((batch.shape[0], batch.shape[1], batch.shape[2], 1))
+
+pred = model.predict(batch, verbose=1, batch_size=32)
+
+predicted_class_indices = np.argmax(pred, axis=1)
+
+for i, sample in enumerate(batch):
+    preds = np.argsort(pred[i])[::-1]
+    str_ = str(i) + '_' + char_list[labels[str(preds[0])]] + str(pred[i][preds[0]])
+    str_ += '_'
+    str_ += char_list[labels[str(preds[1])]] + str(pred[i][preds[1]])
+    str_ += '_'
+    str_ += char_list[labels[str(preds[2])]] + str(pred[i][preds[2]])
+    str_ += '.jpg'
+    cv2.imwrite(dump_folder + str_, batch[i, ...])
+
 
 print('labels', labels)
 
--- text_pic_generator_black_over_white.py
+++ text_pic_generator_black_over_white.py
@@ -4,31 +4,18 @@ import os
 import json
 
 char_list = 'abcdefghijklmnopqrstuvwxyzàâäéèêëîïôùûüœçABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/\\,.;:!?"\'’-_<>()[]{}|&*+=%$µ§°#~@£¤²` '
-with open('char_list.json', 'w') as outfile:
+with open('char_list_2.json', 'w') as outfile:
     json.dump(char_list, outfile)
 nb_classes = len(char_list)
 
 font_folder = 'fonts'
 fonts = os.listdir(font_folder)
 
-dataset_size = 1000 * nb_classes
+dataset_size = 500 * nb_classes
 valid_ratio = 0.25
-valid_size = int(dataset_size * valid_ratio)
 test_ratio = 0.5
-test_size = int(dataset_size * test_ratio)
 
 root_dir = 'dataset_bw'
-train_dir = os.path.join(root_dir, 'train')
-valid_dir = os.path.join(root_dir, 'valid')
-test_dir = os.path.join(root_dir, 'test')
-if not os.path.exists(root_dir):
-    os.makedirs(root_dir)
-if not os.path.exists(train_dir):
-    os.makedirs(train_dir)
-if not os.path.exists(valid_dir):
-    os.makedirs(valid_dir)
-if not os.path.exists(test_dir):
-    os.makedirs(test_dir)
 
 
 def generate_pic(folder, class_, str_, font, font_size, background_color,
@@ -51,7 +38,7 @@ def generate_pic(folder, class_, str_, font, font_size, background_color,
     img.save(os.path.join(class_dir, pic_name + '.png'))
 
 
-def make_dataset(folder, dataset_size):
+def make_dataset_folder(folder, dataset_size):
     for i in range(dataset_size):
         # rand_char_id = np.random.randint(len(char_list))
         # str_ = char_list[rand_char_id]
@@ -66,12 +53,31 @@ def make_dataset(folder, dataset_size):
         rand_background_color = np.random.randint(130, 256, 1)
         background_color = (rand_background_color, rand_background_color,
                             rand_background_color)
-        rand_color = np.random.randint(0, 120, 1)
+        rand_color = np.random.randint(0, 121, 1)
         text_color = (rand_color, rand_color, rand_color)
         generate_pic(folder, rand_char_id, str_, font_path, font_size,
                      background_color, text_color)
         print('generated ' + str(i + 1) + '/' + str(dataset_size) + ' samples')
 
-make_dataset(train_dir, dataset_size)
-make_dataset(valid_dir, valid_size)
-make_dataset(test_dir, test_size)
+
+def make_dataset(root_dir, dataset_size, valid_ratio, test_ratio):
+    train_dir = os.path.join(root_dir, 'train')
+    valid_dir = os.path.join(root_dir, 'valid')
+    test_dir = os.path.join(root_dir, 'test')
+    if not os.path.exists(root_dir):
+        os.makedirs(root_dir)
+    if not os.path.exists(train_dir):
+        os.makedirs(train_dir)
+    if not os.path.exists(valid_dir):
+        os.makedirs(valid_dir)
+    if not os.path.exists(test_dir):
+        os.makedirs(test_dir)
+    valid_size = int(dataset_size * valid_ratio)
+    test_size = int(dataset_size * test_ratio)
+    make_dataset_folder(train_dir, dataset_size)
+    make_dataset_folder(valid_dir, valid_size)
+    make_dataset_folder(test_dir, test_size)
+
+
+if __name__ == '__main__':
+    make_dataset(root_dir, dataset_size, valid_ratio, test_ratio)
